4df804cb03c81584a8c4773c0664bccbaedd4b28
orzalro <45999364+orzalro@users.noreply.github.com>
2025-02-27 18:02:55 +0900

    [Bronze II] Title: Hashing, Time: 44 ms, Memory: 32412 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.py
+++ 백준/Bronze/15829. Hashing/Hashing.py
@@ -4,8 +4,9 @@ l = int(sys.stdin.readline().strip())
 a = sys.stdin.readline().strip()
 
 r = 31
+m = 1234567891
 s = 0
 
 for i in range(l):
     s += (ord(a[i]) - 96) * r ** i
-print(s)
+print(s % 1234567891)
