d849b24c72ea91ed5447697aafaf2ff03dc5cc6a
orzalro <45999364+orzalro@users.noreply.github.com>
2025-03-01 14:15:29 +0900

    [Bronze I] Title: 평균, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/1546. 평균/평균.py
+++ 백준/Bronze/1546. 평균/평균.py
@@ -1,11 +1,7 @@
 import sys
 
 n = int(sys.stdin.readline().strip())
-total = 0
 
 l = list(map(int, sys.stdin.readline().split()))
 
-for i in l:
-    total += i / max(l) * 100
-
-print(total / n)
+print(sum(l) / max(l) * 100 / n)
