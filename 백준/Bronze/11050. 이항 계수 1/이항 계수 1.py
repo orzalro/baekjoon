0828bd975f9c96ccc51e1ff25de43b52dc92de40
orzalro <45999364+orzalro@users.noreply.github.com>
2025-03-05 21:26:43 +0900

    [Bronze I] Title: 이항 계수 1, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/11050. 이항 계수 1/이항 계수 1.py
+++ 백준/Bronze/11050. 이항 계수 1/이항 계수 1.py
@@ -1,6 +1,16 @@
 import sys
-import math
 
 n, k = map(int, (sys.stdin.readline().split()))
 
-print(math.factorial(n) // (math.factorial(k) * math.factorial(n - k)))
+def calc(n, k):
+    list = [[0] * (k + 1) for _ in range(n + 1)]
+
+    for i in range(n + 1):
+        for j in range(min(i, k) + 1):
+            if j == 0 or j == i:
+                list[i][j] = 1
+            else:
+                list[i][j] = list[i - 1][j - 1] + list[i - 1][j]
+    return list[n][k]
+
+print(calc(n, k))
