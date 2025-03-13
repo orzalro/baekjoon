636ff14e61eb3a0693645799d75e275043ad80f0
orzalro <45999364+orzalro@users.noreply.github.com>
2025-03-14 04:43:05 +0900

    [Silver V] Title: 집합, Time: 2620 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/11723. 집합/집합.py
+++ 백준/Silver/11723. 집합/집합.py
@@ -6,23 +6,21 @@ s = []
 
 for i in range(m):
     command = sys.stdin.readline().split()
-    if len(command) > 1:
-        c = int(command[1])
     if command[0] == 'add':
-        if c in s:
+        if command[1] in s:
             continue
-        s.append(c)
+        s.append(command[1])
     elif command[0] == 'remove':
-        if c in s:
-            s.remove(c)
+        if command[1] in s:
+            s.remove(command[1])
     elif command[0] == 'check':
-        print(1 if c in s else 0)
+        print(1 if command[1] in s else 0)
     elif command[0] == 'toggle':
-        if c in s:
-            s.remove(c)
+        if command[1] in s:
+            s.remove(command[1])
         else:
-            s.append(c)
+            s.append(command[1])
     elif command[0] == 'all':
-        s = list(range(1, 21))
+        s = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20']
     elif command[0] == 'empty':
-        s.clear()
+        s = []
