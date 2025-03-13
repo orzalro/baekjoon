ecbb0ec22c4599234361575713e79fd0d55dd987
orzalro <45999364+orzalro@users.noreply.github.com>
2025-03-14 03:28:12 +0900

    [Silver II] Title: 스택 수열, Time: 908 ms, Memory: 41912 KB -BaekjoonHub

--- 백준/Silver/1874. 스택 수열/스택 수열.py
+++ 백준/Silver/1874. 스택 수열/스택 수열.py
@@ -8,30 +8,37 @@ for i in range(n):
     l.append(i + 1)
     target.append(int(sys.stdin.readline().strip()))
 
-stack = []
-poplist = []
-answer = []
+def calc(l, target):
+    stack = []
+    poplist = []
+    answer = []
 
-for i in target:
-    if len(l) > 0 or len(stack):
-        if i < l[0] if len(l) > 0 else stack[-1]:
-            for j in range(len(stack), 0, -1):
-                answer.append('-')
-                if i == stack[j - 1]:
-                    poplist.append(stack.pop())
-                    break
-        else:
-            for j in range(len(l)):
-                j = l.pop(0)
-                stack.append(j)
-                answer.append('+')
-                if i == j:
+    for i in target:
+        if len(l) > 0 or len(stack):
+            if i < l[0] if len(l) > 0 else stack[-1]:
+                flag = 0
+                for j in range(len(stack), 0, -1):
                     answer.append('-')
-                    poplist.append(stack.pop())
-                    break
+                    if i == stack[j - 1]:
+                        flag = 1
+                        poplist.append(stack.pop())
+                        break
+                if flag == 0:
+                    return 'NO'
+            else:
+                flag = 0
+                for j in range(len(l)):
+                    j = l.pop(0)
+                    stack.append(j)
+                    answer.append('+')
+                    if i == j:
+                        flag = 1
+                        answer.append('-')
+                        poplist.append(stack.pop())
+                        break
+                if flag == 0:
+                    return 'NO'
 
-if len(l) == 0 and len(stack) == 0:
-    for i in answer:
-        print(i)
-else:
-    print('NO')
+    return '\n'.join(answer)
+    
+print(calc(l, target))
