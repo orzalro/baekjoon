f0b90ea10c6fba70f6bcf39f18166428afe0c653
orzalro <45999364+orzalro@users.noreply.github.com>
2025-03-23 09:47:29 +0900

    [Silver I] Title: 숨바꼭질, Time: 1232 ms, Memory: 114848 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.py
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.py
@@ -5,30 +5,32 @@ input = sys.stdin.readline
 
 n, k = map(int, input().split())
 
-q = deque()
-q.append(n)
-next_q = deque()
-visited = set()
-depth = 0
-
-while n < k:
-    current = q.popleft()
-    visited.add(current)
-    if current == k:
-        break
-    if current - 1 not in visited and current - 1 <= 100000:
-        next_q.append(current - 1)
-    if current + 1 not in visited and current + 1 <= 100000:
-        next_q.append(current + 1)
-    if current * 2 not in visited and current * 2 <= 100000:
-        next_q.append(current * 2)
+def bfs():
+    q = deque()
+    q.append(n)
+    next_q = deque()
+    visited = set()
+    depth = 0
     
-    if len(q) == 0:
-        q = next_q
-        next_q = deque()
-        depth += 1
+    while n < k:
+        current = q.popleft()
+        visited.add(current)
+        if current == k:
+            break
+        if current - 1 not in visited and current - 1 <= 100000:
+            next_q.append(current - 1)
+        if current + 1 not in visited and current + 1 <= 100000:
+            next_q.append(current + 1)
+        if current * 2 not in visited and current * 2 <= 100000:
+            next_q.append(current * 2)
+        
+        if len(q) == 0:
+            q = next_q
+            next_q = deque()
+            depth += 1
+    return depth
 
 if n > k:
     print(n - k)
 else:
-    print(depth)
+    print(bfs())
