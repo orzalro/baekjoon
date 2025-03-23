9d63cf2bc70df88aeee82e7c5c3c0d1523f9a6a4
orzalro <orzalro@gmail.com>
2025-03-23 13:21:49 +0900

    [Silver I] Title: IOIOI, Time: 124 ms, Memory: 35344 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/5525. IOIOI/IOIOI.py
+++ 백준/Silver/5525. IOIOI/IOIOI.py
@@ -8,21 +8,17 @@ s = input().strip()
 pn = 'IO' * n + 'I'
 pn_len = 2 * n + 1
 
-i = 0
 count = 0
 
 
-while i < s_len - pn_len + 1:
-    if s[i] == 'I':
-        if s[i:i + pn_len] == pn:
-            while s[i + pn_len:i + pn_len + 2] == 'OI':
-                i += 2
-                count += 1
-            i += pn_len
-            count += 1
-        else:
-            i += 1
-    else:
-        i += 1
+while True:
+    i = s.find(pn)
+    if i == -1:
+        break
+    while s[i + pn_len:i + pn_len + 2] == 'OI':
+        i += 2
+        count += 1
+    count += 1
+    s = s[i + pn_len:]
 
 print(count)
