3f34f828c5f34d8c712c2c8e34a9ea792eddf0fb
orzalro <45999364+orzalro@users.noreply.github.com>
2025-04-04 22:31:18 +0900

    [Silver II] Title: N과 M (12), Time: 80 ms, Memory: 36504 KB -BaekjoonHub

---
+++ 백준/Silver/15666. N과 M （12）/N과 M （12）.py
@@ -0,0 +1,37 @@
+import sys
+
+input = sys.stdin.readline
+
+n, m = map(int, (input().split()))
+num = list(map(int, input().split()))
+
+def calc(arr, m):
+    result = []
+    if m < 1:
+        return
+    else:
+        for i in range(0, len(arr)):
+            temp = calc(arr[i:], m - 1)
+            if temp is not None:
+                for j in temp:
+                    result.append([arr[i], j])
+            else: result.append(arr[i])
+        return result
+
+def answer(result):
+    if type(result) is int:
+            return str(result)
+    else:
+        ans = ''
+        for i in range(len(result)):
+            ans = ' '.join([ans, answer(result[i])])
+        return ans.lstrip()
+
+num.sort()
+result = calc(num, m)
+s = set()
+for i in range(len(result)):
+    string = answer(result[i])
+    if string not in s:
+        s.add(string)
+        print(string)
