d713ba73dedf881bc5ec67fe6fdc1a054ae7c3bc
orzalro <45999364+orzalro@users.noreply.github.com>
2025-04-16 04:32:01 +0900

    [Gold V] Title: 평범한 배낭, Time: 592 ms, Memory: 36320 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
@@ -9,12 +9,12 @@ for i in range(n):
     w, v = map(int, input().split())
     p.append([w, v])
 
-dp = [[0 for _ in range(k + 1)] for _ in range(n + 1)]
-for i in range(1, n + 1):
-    for w in range(1, k + 1):
-        if p[i - 1][0] <= w:
-            dp[i][w] = max(dp[i - 1][w], dp[i - 1][w - p[i - 1][0]] + p[i - 1][1])
-        else:
-            dp[i][w] = dp[i - 1][w]
+dp = {0: 0}
+for w, v in p:
+    temp = {}
+    for dp_v, dp_w in dp.items():
+        if dp.get(new_v:= dp_v + v, k + 1) > (new_w := dp_w + w):
+            temp[new_v] = new_w
+    dp.update(temp)
 
-print(dp[-1][-1])
+print(max(dp.keys()))
