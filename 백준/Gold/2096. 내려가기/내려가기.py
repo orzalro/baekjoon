a44009f9f4c9efb2f09f02171d33ca6f8ccea6c3
orzalro <45999364+orzalro@users.noreply.github.com>
2025-04-16 01:58:04 +0900

    [Gold V] Title: 내려가기, Time: 272 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/2096. 내려가기/내려가기.py
+++ 백준/Gold/2096. 내려가기/내려가기.py
@@ -7,8 +7,8 @@ n = int(input())
 dp_max = [0, 0, 0]
 dp_min = [0, 0, 0]
 for _ in range(n):
-    num = list(map(int, input().split()))
-    dp_max = [num[0] + max(dp_max[0:2]), num[1] + max(dp_max[0:3]), num[2] + max(dp_max[1:3])]
-    dp_min = [num[0] + min(dp_min[0:2]), num[1] + min(dp_min[0:3]), num[2] + min(dp_min[1:3])]
+    a, b, c = map(int, input().split())
+    dp_max = [a + max(dp_max[0:2]), b + max(dp_max[0:3]), c + max(dp_max[1:3])]
+    dp_min = [a + min(dp_min[0:2]), b + min(dp_min[0:3]), c + min(dp_min[1:3])]
 
 print(max(dp_max), min(dp_min))
