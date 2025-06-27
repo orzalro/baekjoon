27b8a2e5b0e6c6ba6c52ca9de45e261ef6411814
orzalro <45999364+orzalro@users.noreply.github.com>
2025-06-28 07:31:00 +0900

    [Bronze II] Title: 비슷한 전화번호 표시, Time: 84 ms, Memory: 41340 KB -BaekjoonHub

---
+++ 백준/Bronze/26041. 비슷한 전화번호 표시/비슷한 전화번호 표시.py
@@ -0,0 +1,11 @@
+import sys
+input = sys.stdin.readline
+
+a = list(input().split())
+b = input().strip()
+
+count = 0
+for s in a:
+    if len(s) != len(b) and s[:len(b)] == b:
+        count += 1
+print(count)
