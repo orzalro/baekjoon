9429a7df7cb04ecbb16f76b187766792abba62be
orzalro <45999364+orzalro@users.noreply.github.com>
2025-06-29 07:36:28 +0900

    [Bronze I] Title: Flat Earth, Time: 64 ms, Memory: 35936 KB -BaekjoonHub

---
+++ 백준/Bronze/22986. Flat Earth/Flat Earth.py
@@ -0,0 +1,13 @@
+import sys
+from functools import reduce
+input = sys.stdin.readline
+
+def get_size(n):
+    return (1 + n) * 2 * n
+
+t = int(input())
+for i in range(t):
+    n, k = map(int, input().split())
+    max_start = get_size(n)
+    min_start = get_size(n - k - 1) if n - k > 1 else 0
+    print(max_start - min_start)
