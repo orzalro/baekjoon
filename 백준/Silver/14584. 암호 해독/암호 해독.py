55e37d06faddbfa4f52dbf7131f7a6577663573c
orzalro <45999364+orzalro@users.noreply.github.com>
2025-07-18 17:39:18 +0900

    [Silver IV] Title: 암호 해독, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/14584. 암호 해독/암호 해독.py
+++ 백준/Silver/14584. 암호 해독/암호 해독.py
@@ -10,10 +10,4 @@ for i in range(n):
 flag = 0
 for i in range(1, 27):
     decode_s = ''.join(map(lambda s: chr(s + i) if (s + i) < 123 else chr(s + i - 26), map(ord, s)))
-    for word in d:
-        if word in decode_s:
-            print(decode_s)
-            flag = 1
-            break
-    if flag == 1:
-        break
+    if any(w in decode_s for w in d): print(decode_s); break
