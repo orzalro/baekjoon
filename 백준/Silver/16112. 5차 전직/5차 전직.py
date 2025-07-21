dd965a1c4164bdddab78801272cfb5998a1482f1
orzalro <45999364+orzalro@users.noreply.github.com>
2025-07-21 10:27:00 +0900

    [Silver II] Title: 5차 전직, Time: 224 ms, Memory: 66624 KB -BaekjoonHub

--- 백준/Silver/16112. 5차 전직/5차 전직.py
+++ 백준/Silver/16112. 5차 전직/5차 전직.py
@@ -3,9 +3,5 @@ input = sys.stdin.readline
 
 n, k = map(int, input().split())
 exp_list = list(map(int, input().split()))
-total = 0
 
-for i, exp in enumerate(sorted(exp_list)):
-    total += min(i, k) * exp
-
-print(total)
+print(sum(min(i, k) * exp for i, exp in enumerate(sorted(exp_list))))
