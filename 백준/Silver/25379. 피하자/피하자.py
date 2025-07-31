7553796b5c576c1484ff0a90ebc4b891baa8101c
orzalro <45999364+orzalro@users.noreply.github.com>
2025-07-31 11:32:22 +0900

    [Silver IV] Title: 피하자, Time: 524 ms, Memory: 122208 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/25379. 피하자/피하자.py
+++ 백준/Silver/25379. 피하자/피하자.py
@@ -2,12 +2,15 @@ import sys
 input = sys.stdin.readline
 
 n = int(input())
-a = [*map(int, input().split())]
-count = 0
-left = [i for i, (num1, num2) in enumerate(zip(a, sorted(a, key=lambda x: x % 2)))if num1 != num2]
-right = [i for i, (num1, num2) in enumerate(zip(a, sorted(a, key=lambda x: x % 2, reverse=True)))if num1 != num2]
-if len(left) == 0:
-    left = [0]
-if len(right) == 0:
-    right = [0]
-print(min(max(left) - min(left), max(right) - min(right)))
+a = [i % 2 for i in map(int, input().split())]
+
+def calc(a, num):
+    count = 0
+    left = 0
+    for i, n in enumerate(a):
+        if n == num:
+            count += i - left
+            left += 1
+    return count
+
+print(min(calc(a, 0), calc(a, 1)))
