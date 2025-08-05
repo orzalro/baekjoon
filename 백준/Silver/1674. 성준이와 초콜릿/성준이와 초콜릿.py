cd81ec7d29164e61129be42446206df41dee1144
orzalro <45999364+orzalro@users.noreply.github.com>
2025-08-05 11:02:24 +0900

    [Silver I] Title: 성준이와 초콜릿, Time: 432 ms, Memory: 33432 KB -BaekjoonHub

---
+++ 백준/Silver/1674. 성준이와 초콜릿/성준이와 초콜릿.py
@@ -0,0 +1,33 @@
+import sys
+input = sys.stdin.readline
+
+query = []
+choco = []
+coffee = []
+
+while True:
+    temp = input().split()
+    if temp:
+        if temp[0] == 'Query':
+            query.append(int(temp[1]))
+        elif temp[0] == 'Chocolate':
+            choco.append([int(temp[1]), float(temp[2])])
+        else:
+            coffee.append([int(temp[1]), float(temp[2])])
+    else:
+        break
+
+query.sort()
+for t1 in query:
+    r = 0
+    for t2, n in [[i[0], i[1]]for i in choco if i[0] <= t1]:
+        t = t1 - t2
+        choco_radius = 8 * n - t / 12
+        if choco_radius > 0:
+            r += choco_radius
+    for t2, n in [[i[0], i[1]]for i in coffee if i[0] <= t1]:
+        t = t1 - t2
+        coffee_radius = 2 * n - t * t / 79
+        if coffee_radius > 0:
+            r += coffee_radius
+    print(t1, round(max(1.0, r) + 0.0000001, 1))
