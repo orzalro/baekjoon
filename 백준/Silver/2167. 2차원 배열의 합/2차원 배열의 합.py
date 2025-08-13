65bb40ae70d525740b82e564e44f30d2e7dfb6d4
orzalro <45999364+orzalro@users.noreply.github.com>
2025-08-13 18:19:03 +0900

    [Silver V] Title: 2차원 배열의 합, Time: 104 ms, Memory: 35480 KB -BaekjoonHub

--- 백준/Silver/2167. 2차원 배열의 합/2차원 배열의 합.py
+++ 백준/Silver/2167. 2차원 배열의 합/2차원 배열의 합.py
@@ -2,9 +2,8 @@ import sys
 input = sys.stdin.readline
 
 n, m = map(int, input().split())
-l = [[0 for _ in range(m + 1)]]
-for _ in range(n):
-    l.append([0 ,*map(int, input().split())])
+l = [[0 ,*map(int, input().split())] for _ in range(n)]
+l.insert(0, [0 for _ in range(m + 1)])
 
 for i in range(1, n + 1):
     for j in range(1, m + 1):
