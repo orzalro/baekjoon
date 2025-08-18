84749ab6dee8938e9c5f87b233c873077176a824
orzalro <45999364+orzalro@users.noreply.github.com>
2025-08-18 18:57:39 +0900

    [Gold V] Title: 닭강정의 전설, Time: 2256 ms, Memory: 364904 KB -BaekjoonHub

---
+++ 백준/Gold/30972. 닭강정의 전설/닭강정의 전설.py
@@ -0,0 +1,30 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+k = [[*map(int, input().split())] for _ in range(n)]
+q = int(input())
+l = [(map(int, input().split())) for _ in range(q)]
+
+for i in range(n):
+    for j in range(n):
+        if j > 0:
+            k[i][j] += k[i][j - 1]
+        if i > 0:
+            k[i][j] += k[i - 1][j]
+        if j > 0 and i > 0:
+            k[i][j] -= k[i - 1][j - 1]
+
+for r1, c1, r2, c2 in l:
+    r1, c1, r2, c2 = (r1 - 1, c1 - 1, r2 - 1, c2 - 1)
+    result = -k[r2][c2]
+    if r1 > 0:
+        result += k[r1 -1][c2]
+    if c1 > 0:
+        result += k[r2][c1 - 1]
+    if r1 > 0 and c1 > 0:
+        result -= k[r1 - 1][c1 - 1]
+
+    if r1 + 1 < r2 and c1 + 1 < c2:
+        result += (k[r2 - 1][c2 - 1] - k[r2 - 1][c1] - k[r1][c2 - 1] + k[r1][c1]) * 2
+    print(result)
