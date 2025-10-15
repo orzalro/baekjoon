3eb0b3202517e483bb44bca775c1414aad823231
orzalro <45999364+orzalro@users.noreply.github.com>
2025-10-16 08:13:38 +0900

    [Bronze II] Title: 바구니 뒤집기, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/10811. 바구니 뒤집기/바구니 뒤집기.py
+++ 백준/Bronze/10811. 바구니 뒤집기/바구니 뒤집기.py
@@ -5,8 +5,5 @@ n, m = map(int, input().split())
 basket = [i for i in range(1, n + 1)]
 for _ in range(m):
     i, j = map(int, input().split())
-    left = basket[:i - 1]
-    middle = [*reversed(basket[i - 1:j])]
-    right = basket[j:]
-    basket = left + middle + right
+    basket[i - 1:j] = [*reversed(basket[i - 1:j])]
 print(*basket)
