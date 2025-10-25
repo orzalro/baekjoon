23d84762d116e1ac7e898cb96c27ccd5565f1a0b
orzalro <45999364+orzalro@users.noreply.github.com>
2025-10-25 10:29:35 +0900

    [Bronze I] Title: 세로읽기, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/10798. 세로읽기/세로읽기.py
+++ 백준/Bronze/10798. 세로읽기/세로읽기.py
@@ -5,8 +5,6 @@ s_l = [input().strip() for _ in range(5)]
 answer = []
 for i in range(15):
     for s in s_l:
-        try:
+        if len(s) > i:
             answer.append(s[i])
-        except:
-            continue
 print(*answer, sep='')
