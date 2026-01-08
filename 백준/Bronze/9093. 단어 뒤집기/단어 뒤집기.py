dc56b1f28ce377f3356afc67852a79cac29efc0b
orzalro <45999364+orzalro@users.noreply.github.com>
2026-01-08 18:35:48 +0900

    [Bronze I] Title: 단어 뒤집기, Time: 116 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/9093. 단어 뒤집기/단어 뒤집기.py
+++ 백준/Bronze/9093. 단어 뒤집기/단어 뒤집기.py
@@ -3,6 +3,5 @@ input = sys.stdin.readline
 
 t = int(input())
 for _ in range(t):
-    for word in input().split():
-        print(word[::-1], end=' ')
+    [print(word[::-1], end=' ') for word in input().split()]
     print()
