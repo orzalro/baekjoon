fb7b7820c437311d8155c470cd2499ba47c88ab2
orzalro <45999364+orzalro@users.noreply.github.com>
2026-02-16 13:16:36 +0900

    [Gold V] Title: 별 찍기 - 10, Time: 56 ms, Memory: 36632 KB -BaekjoonHub

--- 백준/Gold/2447. 별 찍기 － 10/별 찍기 － 10.py
+++ 백준/Gold/2447. 별 찍기 － 10/별 찍기 － 10.py
@@ -1,18 +1,21 @@
-def calc(n, c='*'):
+def calc(n):
     if n == 1:
-        return c
-    else:
-        block = calc(n // 3, c)
-        blank = calc(n // 3, ' ')
-        
-        result = []
-        for row in block:
-            result.append(row * 3)
-        for row in zip(block, blank, block):
-            result.append(''.join(row))
-        for row in block:
-            result.append(row * 3)
-        return result
+        return '*'
+    
+    prev = calc(n // 3)
+    result = []
+
+    for row in prev:
+        result.append(row * 3)
+
+    for row in prev:
+        result.append(row + ' ' * (n // 3) + row)
+
+    for row in prev:
+        result.append(row * 3)
+    
+    return result
+
 
 n = int(input())
 print(*calc(n), sep='\n')
