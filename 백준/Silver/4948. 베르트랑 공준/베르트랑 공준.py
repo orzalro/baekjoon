98f4a0a8ebe5ff239b25bfea4f8104b25ea1544c
orzalro <45999364+orzalro@users.noreply.github.com>
2026-03-10 16:02:14 +0900

    [Silver II] Title: 베르트랑 공준, Time: 608 ms, Memory: 46248 KB -BaekjoonHub

---
+++ 백준/Silver/4948. 베르트랑 공준/베르트랑 공준.py
@@ -0,0 +1,24 @@
+import sys
+input = sys.stdin.readline
+
+def eratosthenes(num):
+    prime = set(range(5, num + 1, 6)) | set(range(7, num + 1, 6))
+    if num > 1: prime.add(2)
+    if num > 2: prime.add(3)
+
+    for i in range(5, int(num ** 0.5) + 1, 6):
+        j = i + 2
+        if i in prime:
+            prime -= set(range(i * i, num + 1, i * 6)) | set(range(i * (i + 2), num + 1, i * 6))
+        if j in prime:
+            prime -= set(range(j * j, num + 1, j * 6)) | set(range(j * (j + 4), num + 1, j * 6))
+
+    return prime
+
+prime = eratosthenes(123456 * 2)
+
+while True:
+    n = int(input())
+    if n == 0: break
+
+    print(len([num for num in prime if num > n and num <= n * 2]))
