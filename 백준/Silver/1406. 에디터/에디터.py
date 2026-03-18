c0dac1a45b0bf18a65641b5fa9bd630cee112e57
orzalro <45999364+orzalro@users.noreply.github.com>
2026-03-18 11:12:44 +0900

    [Silver II] Title: 에디터, Time: 228 ms, Memory: 38732 KB -BaekjoonHub

--- 백준/Silver/1406. 에디터/에디터.py
+++ 백준/Silver/1406. 에디터/에디터.py
@@ -1,20 +1,21 @@
 import sys
-from collections import deque
 input = sys.stdin.readline
 
-left = deque(input().strip())
-right = deque()
+left = list(input().strip())
+right = []
 m = int(input())
 
 for _ in range(m):
     command = input().split()
     if command[0] == 'L' and left:
-        right.appendleft(left.pop())
+        right.append(left.pop())
     elif command[0] == 'D' and right:
-        left.append(right.popleft())
+        left.append(right.pop())
     elif command[0] == 'B' and left:
         left.pop()
     elif command[0] == 'P':
         left.append(command[1])
 
-print(*left, *right, sep='')
+right.reverse()
+left.extend(right)
+print(''.join(left))
