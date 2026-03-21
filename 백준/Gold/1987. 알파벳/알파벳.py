2f5c299cbfe6e743f704ad4cd60d719e71e4c0e6
orzalro <45999364+orzalro@users.noreply.github.com>
2026-03-21 09:18:58 +0900

    [Gold IV] Title: 알파벳, Time: 4216 ms, Memory: 142752 KB -BaekjoonHub

---
+++ 백준/Gold/1987. 알파벳/알파벳.py
@@ -0,0 +1,27 @@
+import sys
+input = sys.stdin.readline
+
+r, c = map(int, input().split())
+board = [input().strip() for _ in range(r)]
+
+dx = [1, -1, 0, 0]
+dy = [0, 0, 1, -1]
+
+def calc(x, y, visited):
+    cnt = 0
+
+    for i in range(4):
+        nx = x + dx[i]
+        ny = y + dy[i]
+
+        if 0 <= nx < c and 0 <= ny < r:
+            char = ord(board[ny][nx]) - 65
+
+            if visited & (1 << char):
+                continue
+
+            cnt = max(cnt, calc(nx, ny, visited | (1 << char)))
+
+    return cnt + 1
+
+print(calc(0, 0, 1 << (ord(board[0][0]) - 65)))
