bd6cb72be7947480d2f3341741c27c19ff7bfe6c
orzalro <45999364+orzalro@users.noreply.github.com>
2026-03-22 21:09:05 +0900

    [Gold IV] Title: 동전 1, Time: 188 ms, Memory: 33432 KB -BaekjoonHub

--- 백준/Gold/2293. 동전 1/동전 1.py
+++ 백준/Gold/2293. 동전 1/동전 1.py
@@ -2,15 +2,12 @@ import sys
 input = sys.stdin.readline
 
 n, k = map(int, input().split())
-coins = []
-for _ in range(n):
-    price = int(input())
-    if price <= k:
-        coins.append(price)
+coins = [int(input()) for _ in range(n)]
 
 dp = [0 for _ in range(k + 1)]
+dp[0] = 1
+
 for coin in coins:
-    dp[coin] += 1
     for i in range(coin, k + 1):
         dp[i] += dp[i - coin]
 
